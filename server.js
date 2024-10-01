cfebe61269a452467ee5eb310c250f595722f833
sportdogfood <philip@sportdogfood.com>
2024-10-01 10:49:12 -0400

    Added dynamic FoxyCart API handling

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ async function refreshZohoToken() {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams({
         grant_type: 'refresh_token',
-        refresh_token: '1000.b2eed1d9e51d611968fe09e31b20e9be.0d32a603a75f7c6c33ccbf6465ca2649',  // Your Zoho refresh token
+        refresh_token: '1000.4f7761b990e65f8b077a2a98d9489316.da36bf3213cfd20f4e7c92fb1b243148',  // Your Zoho refresh token
         client_id: '1000.3VZRY3CC9QGZBXA8IZZ6TWZTZV1H6H',  // Your Zoho client ID
         client_secret: '48dcd0b587246976e9dfcfcc54b10bfb211686cbe4',  // Your Zoho client secret
       }),
