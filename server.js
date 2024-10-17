005f359b9e05ed877bb16f0745a9a04ff634166d
sportdogfood <philip@sportdogfood.com>
2024-10-16 22:22:08 -0400

    Fix email

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, fx.customer');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, fx-customer');
+  
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
   next();
 });
+
 
 // Handle OPTIONS preflight requests
 app.options('*', (req, res) => {
